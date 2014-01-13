6e874f732a3f51f09cdeb61321cef168678f1a81
Tom Stuart <tom@codon.com>
2014-01-13 16:34:46 +0000

    Set up spec_helper to require all support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
+
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
