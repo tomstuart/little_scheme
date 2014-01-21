12583835ba85c5429ef637bf67e304f69d674284
thattommyhall <thattommyhall@gmail.com>
2014-01-21 00:44:00 +0000

    Still with classes

--- simple.rb
+++ simple.rb
@@ -1,6 +1,30 @@
 require 'treetop'
+
+class Sexp < Treetop::Runtime::SyntaxNode
+end
+
+class Atom < Sexp
+  def to_a
+    text_value
+  end
+end
+
+class List < Sexp
+  def to_a
+    p elements
+  end
+end
+
+
 Treetop.load('scheme.treetop')
 
 parser = SchemeParser.new
 
-p parser.parse("(foo bar)")
+
+
+tree = parser.parse("(foo)")
+
+
+p tree
+
+p tree.to_a
