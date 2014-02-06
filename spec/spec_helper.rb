1568827f605f96242e745d9130b80af686e15681
Tom Stuart <tom@codon.com>
2014-02-06 11:24:57 +0000

    Enable :focus filtering for examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,12 @@ RSpec.configure do |config|
   # Limit the spec run to only specs with the focus metadata. If no specs have
   # the filtering metadata and `run_all_when_everything_filtered = true` then
   # all specs will run.
-  #config.filter_run :focus
+  config.filter_run :focus
 
   # Run all specs when none match the provided filter. This works well in
   # conjunction with `config.filter_run :focus`, as it will run the entire
   # suite when no specs have `:filter` metadata.
-  #config.run_all_when_everything_filtered = true
+  config.run_all_when_everything_filtered = true
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
