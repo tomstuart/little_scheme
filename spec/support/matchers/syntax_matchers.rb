d744e34365d1735486b80c1901b682b061dfbeda
Tom Stuart <tom@codon.com>
2014-02-24 18:27:44 +0000

    Add examples for chapter six

--- spec/support/matchers/syntax_matchers.rb
+++ spec/support/matchers/syntax_matchers.rb
@@ -45,4 +45,20 @@ module SyntaxMatchers
         s_expressions.first.s_expressions.all? { |s_expression| s_expression.atom? && s_expression.number? }
     end
   end
+
+  matcher :be_an_arithmetic_expression do
+    def arithmetic_expression?(s_expressions)
+      if s_expressions.length == 1 && s_expressions.first.atom?
+        true
+      elsif s_expressions.length >= 3
+        first, op, *rest = s_expressions
+        arithmetic_expression?([first]) && arithmetic_expression?(rest) && op.atom? && %w(+ * expt).include?(op.name)
+      end
+    end
+
+    match do |string|
+      s_expressions = parse_program(string).s_expressions
+      arithmetic_expression?(s_expressions)
+    end
+  end
 end
