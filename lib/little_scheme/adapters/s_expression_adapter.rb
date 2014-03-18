711ca1d7062fb788dcdc890d4b04ce9006028523
Tom Stuart <tom@codon.com>
2014-03-18 13:35:01 +0000

    Remove unused #atom? and #list? from SExpressionAdapter

--- lib/little_scheme/adapters/s_expression_adapter.rb
+++ lib/little_scheme/adapters/s_expression_adapter.rb
@@ -1,18 +1,9 @@
 require 'delegate'
 require 'evaluator'
-require 'list'
 
 module LittleScheme
   module Adapters
     class SExpressionAdapter < SimpleDelegator
-      def atom?
-        __getobj__.atom?
-      end
-
-      def list?
-        __getobj__.is_a?(List)
-      end
-
       def is_a?(klass)
         __getobj__.is_a?(klass)
       end
