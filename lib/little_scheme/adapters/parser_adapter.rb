9a842a6c41d26fc2c3298f1e29d498339bd9e2b5
Tom Stuart <tom@codon.com>
2014-03-18 13:48:30 +0000

    Remove useless SExpressionAdapter

--- lib/little_scheme/adapters/parser_adapter.rb
+++ lib/little_scheme/adapters/parser_adapter.rb
@@ -1,6 +1,5 @@
 require 'atom'
 require 'list'
-require 'little_scheme/adapters/s_expression_adapter'
 require 'treetop'
 Treetop.load(File.expand_path('../../../scheme.treetop', __FILE__))
 
@@ -8,8 +7,7 @@ module LittleScheme
   module Adapters
     class ParserAdapter
       def parse(string)
-        s_expression = SchemeParser.new.parse(string).to_ast
-        SExpressionAdapter.new(s_expression)
+        SchemeParser.new.parse(string).to_ast
       end
     end
   end
