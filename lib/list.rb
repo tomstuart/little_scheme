tomstuart/little_scheme

commit 67b40823bf4a2efc5ef73d253f83d3e0db7a1503

Remove duplication in immediate evaluation of lambda

lib/list.rb
@@ -9,10 +9,9 @@ class List
     function, *arguments = array
     if function.is_a? List
       expression = function.cdr.cdr.car
-      parameter_name = function.cdr.car.car.symbol
-      local_env = env.merge(parameter_name => arguments.first.evaluate(env))
+      parameter_name = function.cdr.car.car
 
-      expression.evaluate(local_env)
+      Lambda.new(parameter_name, expression).evaluate(env, arguments.first.evaluate(env))
     else
       operation = function.symbol
       if env.key?(operation)

commit 85b6f0dd1458459921a20dcdbd8bfa9294b90fce

Fix immediate evaluation of lambda
Which we broke in the refactoring in 67b40823bf4a2efc5ef73d253f83d3e0db7a1503

lib/list.rb
@@ -11,7 +11,7 @@ class List
       expression = function.cdr.cdr.car
       parameter = function.cdr.car.car
 
-      Lambda.new(parameter, expression).evaluate(env, arguments.first.evaluate(env))
+      Lambda.new(parameter, expression).evaluate(env, arguments.first)
     else
       operation = function.symbol
       if env.key?(operation)