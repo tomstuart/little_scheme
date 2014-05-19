ddc40508cdb7e73726c182dc46f90d1cfcccfb71
Tom Stuart <tom@codon.com>
2014-05-19 19:30:44 +0100

    Introduce Keyword class
    In each case, we just need to turn calls to #evaluate into calls to
    the #call method, and that's all that Keyword does.

--- lib/evaluator.rb
+++ lib/evaluator.rb
@@ -1,33 +1,31 @@
 class Evaluator
-  class Lambda
-    def evaluate(env, arguments)
-      parameters = arguments.first.array
-      expression = arguments.last
-
-      ::Lambda.new(parameters, expression)
+  class Keyword
+    def initialize(&block)
+      @block = block
     end
-  end
 
-  class Quote
     def evaluate(env, arguments)
-      arguments.first
-    end
-  end
-
-  class Cond
-    def evaluate(env, arguments)
-      arguments.detect { |list| list.car.evaluate(env) == Atom::TRUE }.cdr.car.evaluate(env)
-    end
-  end
-
-  class Or
-    def evaluate(env, arguments)
-      arguments.first.evaluate(env) == Atom::TRUE ? Atom::TRUE : arguments.last.evaluate(env)
+      @block.call(env, arguments)
     end
   end
 
   def evaluate(program, env={})
-    env_with_primitives = env.merge lambda: Lambda.new, quote: Quote.new, cond: Cond.new, or: Or.new
+    env_with_primitives = env.merge \
+      lambda: Keyword.new { |env, arguments|
+        parameters = arguments.first.array
+        expression = arguments.last
+
+        Lambda.new(parameters, expression)
+      },
+      quote: Keyword.new { |env, arguments|
+        arguments.first
+      },
+      cond: Keyword.new { |env, arguments|
+        arguments.detect { |list| list.car.evaluate(env) == Atom::TRUE }.cdr.car.evaluate(env)
+      },
+      or: Keyword.new { |env, arguments|
+        arguments.first.evaluate(env) == Atom::TRUE ? Atom::TRUE : arguments.last.evaluate(env)
+      }
 
     program.evaluate(env_with_primitives)
   end
