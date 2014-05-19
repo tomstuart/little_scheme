8ea8b498521abe41c44030b3c88bd082e7f496b6
Tom Stuart <tom@codon.com>
2014-05-19 19:57:44 +0100

    Extract KEYWORDS, PRIMITIVES and INITIAL_ENVIRONMENT constants from Evaluator#evaluate

--- lib/evaluator.rb
+++ lib/evaluator.rb
@@ -20,30 +20,36 @@ class Evaluator
     end
   end
 
+  KEYWORDS = {
+    lambda: Keyword.new { |env, arguments|
+      parameters = arguments.first.array
+      expression = arguments.last
+
+      Lambda.new(parameters, expression)
+    },
+    quote: Keyword.new { |env, arguments|
+      arguments.first
+    },
+    cond: Keyword.new { |env, arguments|
+      arguments.detect { |list| list.car == Atom.new(:else) || list.car.evaluate(env) == Atom::TRUE }.cdr.car.evaluate(env)
+    },
+    or: Keyword.new { |env, arguments|
+      arguments.first.evaluate(env) == Atom::TRUE ? Atom::TRUE : arguments.last.evaluate(env)
+    }
+  }
+
+  PRIMITIVES = {
+    car: Primitive.new(:car),
+    cdr: Primitive.new(:cdr),
+    cons: Primitive.new(:cons),
+    null?: Primitive.new(:null?),
+    atom?: Primitive.new(:atom?),
+    eq?: Primitive.new(:eq?)
+  }
+
+  INITIAL_ENVIRONMENT = KEYWORDS.merge(PRIMITIVES)
+
   def evaluate(program, env={})
-    env_with_primitives = env.merge \
-      lambda: Keyword.new { |env, arguments|
-        parameters = arguments.first.array
-        expression = arguments.last
-
-        Lambda.new(parameters, expression)
-      },
-      quote: Keyword.new { |env, arguments|
-        arguments.first
-      },
-      cond: Keyword.new { |env, arguments|
-        arguments.detect { |list| list.car == Atom.new(:else) || list.car.evaluate(env) == Atom::TRUE }.cdr.car.evaluate(env)
-      },
-      or: Keyword.new { |env, arguments|
-        arguments.first.evaluate(env) == Atom::TRUE ? Atom::TRUE : arguments.last.evaluate(env)
-      },
-      car: Primitive.new(:car),
-      cdr: Primitive.new(:cdr),
-      cons: Primitive.new(:cons),
-      null?: Primitive.new(:null?),
-      atom?: Primitive.new(:atom?),
-      eq?: Primitive.new(:eq?)
-
-    program.evaluate(env_with_primitives)
+    program.evaluate(INITIAL_ENVIRONMENT.merge(env))
   end
 end
