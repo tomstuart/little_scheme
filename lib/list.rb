d21a74483c13fdf27fc11a6bd17272aca56362d7
Tom Stuart <tom@codon.com>
2014-05-19 19:07:38 +0100

    Remove support for evaluating lists that start with a list
    This is making our implementation of List#evaluate more complicated, but
    it's not even in the book, so we're parking it for now.

--- lib/list.rb
+++ lib/list.rb
@@ -7,32 +7,26 @@ class List
 
   def evaluate(env)
     function, *arguments = array
-    if function.is_a? List
-      parameters = function.cdr.car.array
-      expression = function.cdr.cdr.car
 
-      Lambda.new(parameters, expression).evaluate(env, arguments)
+    operation = function.symbol
+    if env.key?(operation)
+      env[operation].evaluate(env, arguments)
     else
-      operation = function.symbol
-      if env.key?(operation)
-        env[operation].evaluate(env, arguments)
-      else
-        case operation
-        when :lambda
-          parameters = arguments.first.array
-          expression = arguments.last
+      case operation
+      when :lambda
+        parameters = arguments.first.array
+        expression = arguments.last
 
-          Lambda.new(parameters, expression)
-        when :quote
-          arguments.first
-        when :cond
-          arguments.detect { |list| list.car.evaluate(env) == Atom::TRUE }.cdr.car.evaluate(env)
-        when :or
-          arguments.first.evaluate(env) == Atom::TRUE ? Atom::TRUE : arguments.last.evaluate(env)
-        else
-          first_argument, *other_arguments = arguments.map { |a| a.evaluate(env) }
-          first_argument.send(operation, *other_arguments)
-        end
+        Lambda.new(parameters, expression)
+      when :quote
+        arguments.first
+      when :cond
+        arguments.detect { |list| list.car.evaluate(env) == Atom::TRUE }.cdr.car.evaluate(env)
+      when :or
+        arguments.first.evaluate(env) == Atom::TRUE ? Atom::TRUE : arguments.last.evaluate(env)
+      else
+        first_argument, *other_arguments = arguments.map { |a| a.evaluate(env) }
+        first_argument.send(operation, *other_arguments)
       end
     end
   end
